020385360bde584836fe6496de135bb69cb12d56
Andrey-Zhiryakov <andrey.zhiriakov@gmail.com>
2016-01-27 00:33:13 +0300

    make removing node without children

--- binary-tree.js
+++ binary-tree.js
@@ -75,6 +75,49 @@ class BinaryTree {
 	}
 
 	remove(data) {
+		if (this.isEmpty() || !this.contains(data)) {
+			return;
+		} 
+
+		var currentNode = this.root;
+		var parentNode = null;
+
+		while (true) {							//search for required node								
+			if (currentNode.left.data === data) {
+				parentNode = currentNode;
+				currentNode = currentNode.left;
+				break;
+			} else if (currentNode.right.data === data) {
+				parentNode = currentNode;
+				currentNode = currentNode.right;
+				break;
+			} else {
+				if (data < currentNode.data) {					//this 'if' statement search for lowest node element
+					if (currentNode.left !== null) {
+						currentNode = currentNode.left;
+					} 
+				}
+
+				if (data > currentNode.data) {					//this 'if' statement search for highest node element
+					if (currentNode.right !== null) {
+						currentNode = currentNode.right;
+					} 
+				}
+			}
+		}
+
+		//delete node without children
+		if (currentNode.left === null && currentNode.right === null) {
+			//debugger;
+			if (currentNode.data < parentNode.data) {
+				parentNode.left = null;
+			} else {
+				parentNode.right = null;
+			}
+			return;
+		}
+
+		//delete node with 1 child
 
 	}
 
